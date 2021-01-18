d378ca989f5f3f7872d4797667d549ffd2185ad2
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2021-01-18 07:51:36 +0530

    Roll the die to get a number between 1 to 6

---
+++ JS/lib.js
@@ -0,0 +1,3 @@
+module.exports.rollDice = () => {
+    return Math.floor(Math.random() *  10) % 6 + 1;
+    }
--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,7 @@
+const lib = require("./lib")
+
 console.log("Welcome in Snake And Ladder Simulator Program")
 
 let playerOnePosition = 0;
+let random = lib.rollDice();
+console.log(random)
